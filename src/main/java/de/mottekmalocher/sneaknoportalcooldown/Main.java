24504d100f0a13d8d655b02f1ff700f6da4276f5
Marc Lürbke <marc.luerbke@gmail.com>
2021-06-13 19:25:02 +0200

    update 1.17

--- src/main/java/de/mottekmalocher/sneaknoportalcooldown/Main.java
+++ src/main/java/de/mottekmalocher/sneaknoportalcooldown/Main.java
@@ -1,18 +1,16 @@
 package de.mottekmalocher.sneaknoportalcooldown;
 
-import net.minecraft.server.v1_16_R3.EntityPlayer;
-import net.minecraft.server.v1_16_R3.PlayerAbilities;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Abilities;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.lang.reflect.Field;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -37,20 +35,9 @@ public class Main extends JavaPlugin implements Listener {
     }
 
     public void setInvulnerable(Player player, boolean bool) {
-        EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
-
-        try {
-            PlayerAbilities playerAbilities = entityPlayer.abilities;
-            Field field = playerAbilities.getClass().getDeclaredField("isInvulnerable");
-
-            field.setAccessible(true);
-            field.setBoolean(playerAbilities, bool);
-            field.setAccessible(false);
-
-            entityPlayer.updateAbilities();
-        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-            System.err.println(e);
-        }
+        ServerPlayer entityPlayer = ((CraftPlayer) player).getHandle();
+        Abilities playerAbilities = entityPlayer.getAbilities();
+        playerAbilities.invulnerable = bool;
     }
 
 }
